f283aaaf9acae385747016bd31281c0c32a9953d
Coridyn Fitzgerald-Hood <coridyn@gmail.com>
2014-08-24 11:58:35 +1000

    refactor(execute): split 'execute' method into smaller parts; raise 'commandComplete' event when done

--- utils/execute.js
+++ utils/execute.js
@@ -5,7 +5,8 @@ var validateCommandOptions = require('./validateCommandOptions'),
 
 module.exports = exports = function (cmdStr, contextData, options) {
 
-    var shell = this;
+    var shell = this,
+        asyncCmd = false;
 
     if (contextData) shell.context.data = contextData;
 
@@ -19,60 +20,30 @@ module.exports = exports = function (cmdStr, contextData, options) {
         shell.emit('done', false);
         return shell;
     }
-
-    // Parse the command string into an argument array and set the command name to the first item.
-    var args = cmdStr,
-        cmdName = cmdStr,
-        asyncCmd = false; 
-    if (cmdStr.length > 0) {
-        args = shellQuote.parse(cmdStr);
-        cmdName = args[0];
-    }
+    
+    
 
     if (cmdStr.toLowerCase() !== 'cancel') {
+        // Parse the args first.
+        var cmdInfo = exports.parseCmdStr.call(this, cmdStr);
+        
         // If a prompt context exists then override command and options with those stored in the context...
         if (prompt) {
-            cmdName = prompt.cmd;
+            cmdInfo.cmdName = prompt.cmd.toLowerCase();
             options = prompt.options;
             options[prompt.option] = cmdStr;
             shell.clearPrompt();
         }
-
-        // Get reference to the command module by name.
-        var cmd = shell.cmds[cmdName.toLowerCase()];
-
-        // ...otherwise remove the command name from the args array and build our options object.
-        if (!prompt) {
-            args.splice(0, 1);
-            // Configure yargs based on defined command options.
-            if (cmd && cmd.hasOwnProperty('options')) {
-                for (var key in cmd.options) {
-                    if (cmd.options.hasOwnProperty(key)) {
-                        var option = cmd.options[key];
-                        if (option.hasOwnProperty('type'))
-                            switch(option.type.toLowerCase()) {
-                                case "string":
-                                    yargs.string(key);
-                                    if (option.hasOwnProperty('aliases'))
-                                        yargs.string(option.aliases);
-                                    break;
-                                case "boolean":
-                                    yargs.boolean(key);
-                                    if (option.hasOwnProperty('aliases'))
-                                        yargs.boolean(option.aliases);
-                            }
-                    }
-                }
-            }
-            // Set options by extending the parsed user options with the manually supplied options.
-            options = extend({}, yargs.parse(args), options);
-        }
-
-        // If the command module exists then process it's options and invoke the module.
-        if (cmd && cmd.access(shell, cmdName.toLowerCase())) {
-            if (options.hasOwnProperty('?') || options.hasOwnProperty('help'))
-                shell.execute('help', contextData, { command: cmdName });
-            else if (validateCommandOptions(options, cmd, shell)) {
+        
+        var cmd = exports.getCmd.call(this, cmdInfo, prompt, contextData, options);
+        if (cmd){
+            options = exports.mergeCommandOptions.call(this, cmd, cmdInfo, options, prompt);
+            
+            if (options.hasOwnProperty('?') || options.hasOwnProperty('help')){
+                shell.execute('help', contextData, { command: cmd.name });
+            } else if (validateCommandOptions(options, cmd, shell)) {
+                // Coridyn: TODO: Raise a start/progress event?
+                
                 try {
                     if (cmd.invoke.length === 3) {
                         // This is an async command module so set asyncCmd to true.
@@ -81,17 +52,23 @@ module.exports = exports = function (cmdStr, contextData, options) {
                         // the done event when it is called.
                         cmd.invoke(shell, options, function (err) {
                             if (err) shell.error(err);
-                            shell.emit('done', !!shell.context.getVar('prompt')); // !! ensures the value is a boolean.
+                            
+                            // Coridyn: Emit an event indicating the command is complete.
+                            exports.commandDone.call(this, shell, cmdInfo, contextData, options);
                         });
-                    } else
+                    } else {
                         // Invoke is not asynchronous so do not pass in a callback.
                         cmd.invoke(shell, options);
+                    }
                 } catch (err) {
                     shell.error(err);
                 }
             }
-        } else
-            shell.log('"' + cmdName + '" is not a valid command', { type: 'error' });
+            
+        } else {
+            shell.log('"' + cmdInfo.cmdName + '" is not a valid command', { type: 'error' });
+        }
+       
     } else {
         // If prompt exists then cancel it...
         if (prompt){
@@ -99,11 +76,141 @@ module.exports = exports = function (cmdStr, contextData, options) {
             shell.clearPrompt();
         }
         // ...otherwise inform user there is no active prompt.
-        else
+        else {
             shell.log('there are no active prompts', { type: 'warn' });
+        }
     }
 
     // Emit a done event if the command was not asynchronous.
-    if (!asyncCmd) shell.emit('done', !!shell.context.getVar('prompt')); // !! ensures the value is a boolean.
+    if (!asyncCmd) {
+        exports.commandDone.call(this, shell, cmdInfo, contextData, options);
+    }
+    
     return shell;
 };
+
+
+/**
+ * Indicate that the command was invoked and has now completed.
+ * 
+ * There could be an error...
+ * 
+ * @param  {[type]} shell [description]
+ * @return {[type]}       [description]
+ */
+exports.commandDone = function(shell, cmdInfo, contextData, options){
+    var hasNewPrompt = !!shell.context.getVar('prompt');
+    shell.emit('done', hasNewPrompt); // !! ensures the value is a boolean.
+    
+    // 2014-08-24:
+    // Raise an event indicating that the command was invoked.
+    // 
+    // Only raise this event when there are no more prompts.
+    if (hasNewPrompt === false){
+        shell.emit('commandComplete', cmdInfo, contextData, options);
+    }
+}
+
+
+/**
+ * Parse the command string into a consistent format.
+ *  {
+ *    cmdName: String,
+ *    args: [all other properties]
+ *  }
+ * 
+ * @return {[type]} [description]
+ */
+exports.parseCmdStr = function(cmdStr){
+    // Parse the command string into an argument array and set the command name to the first item.
+    var args = cmdStr,
+        cmdName = cmdStr;
+    if (cmdStr.length > 0) {
+        args = shellQuote.parse(cmdStr);
+        cmdName = args[0];
+    }
+    
+    // TODO: Return something here.
+    return {
+        cmdName: cmdName.toLowerCase(),
+        args: args
+    }
+}
+
+
+/**
+ * Get the command instance represented by the given cmdStr.
+ * 
+ * @param  {[type]} cmdStr [description]
+ * @param  {[type]} prompt [description]
+ * @return {[type]}        [description]
+ */
+exports.getCmd = function(cmdInfo, prompt, contextData, options){
+    var shell = this;
+    
+    // // Parse the command string into an argument array and set the command name to the first item.
+    // var args = cmdStr,
+    //     cmdName = cmdStr;
+    // if (cmdStr.length > 0) {
+    //     args = shellQuote.parse(cmdStr);
+    //     cmdName = args[0];
+    // }
+    
+    // // If a prompt context exists then override command and options with those stored in the context...
+    // if (prompt) {
+    //     cmdName = prompt.cmd;
+    //     options = prompt.options;
+    //     options[prompt.option] = cmdStr;
+    //     shell.clearPrompt();
+    // }
+
+    // Get reference to the command module by name.
+    var cmd = shell.cmds[cmdInfo.cmdName];
+
+    // If the command module exists then process it's options and invoke the module.
+    if (cmd && cmd.access(shell, cmdInfo.cmdName)) {
+        return cmd;
+    }
+    
+    return undefined;
+}
+
+
+/**
+ * Consolidate the options for the given command.
+ * 
+ * @param  {[type]} cmd    [description]
+ * @param  {[type]} cmdInfo   [description]
+ * @param  {[type]} prompt [description]
+ * @return {[type]}        [description]
+ */
+exports.mergeCommandOptions = function(cmd, cmdInfo, options, prompt){
+    // ...otherwise remove the command name from the args array and build our options object.
+    if (!prompt) {
+        cmdInfo.args.splice(0, 1);
+        // Configure yargs based on defined command options.
+        if (cmd && cmd.hasOwnProperty('options')) {
+            for (var key in cmd.options) {
+                if (cmd.options.hasOwnProperty(key)) {
+                    var option = cmd.options[key];
+                    if (option.hasOwnProperty('type'))
+                        switch(option.type.toLowerCase()) {
+                            case "string":
+                                yargs.string(key);
+                                if (option.hasOwnProperty('aliases'))
+                                    yargs.string(option.aliases);
+                                break;
+                            case "boolean":
+                                yargs.boolean(key);
+                                if (option.hasOwnProperty('aliases'))
+                                    yargs.boolean(option.aliases);
+                        }
+                }
+            }
+        }
+        // Set options by extending the parsed user options with the manually supplied options.
+        options = extend({}, yargs.parse(cmdInfo.args), options);
+    }
+    
+    return options;
+}
